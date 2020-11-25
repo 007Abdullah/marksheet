d7e82502aff531f21b87abef7c6399a0dd295457
Muhammad Abdullah <kb337137@gmail.com>
2020-11-25 05:10:51 +0500

    marksheet

---
+++ mark.js
@@ -0,0 +1,40 @@
+function cals() {
+    let tmarkOne = parseFloat(document.getElementById("U-num").value);
+    let tmarkTwo = parseFloat(document.getElementById("C-num").value);
+    let tmarkThree = parseFloat(document.getElementById("B-num").value);
+    let tmarkFour = parseFloat(document.getElementById("M-num").value);
+    let tmarkFive = parseFloat(document.getElementById("E-num").value);
+
+    let T_add = tmarkOne + tmarkTwo + tmarkThree + tmarkFour + tmarkFive;
+
+    document.getElementById("Add").innerHTML = T_add;
+
+    let Urdu = parseFloat(document.getElementById("uduResult").value);
+    let Chemistry = parseFloat(document.getElementById("chemResult").value);
+    let Biology = parseFloat(document.getElementById("bioResult").value);
+    let Math = parseFloat(document.getElementById("mathResult").value);
+    let English = parseFloat(document.getElementById("engResult").value);
+    let obt = Urdu + Chemistry + Biology + Math + English;
+    document.getElementById("Obtn").innerHTML = obt;
+    let per = obt / T_add * 100;
+    document.getElementById("Percentage").innerHTML = per.toFixed(2);
+
+    if (per >= 60 && per <= 70)  {
+        document.getElementById("Grade").innerText = "C";
+
+    }    
+    else if (per >= 70 && per <= 80) {
+        document.getElementById("Grade").innerText = "B";
+    }
+    
+    else if (per >= 80 && per <= 90) {
+        document.getElementById("Grade").innerText = "A";
+    }
+    
+    else if (per >= 100) {
+        document.getElementById("Grade").innerText = "A+1";
+    }
+    else{
+        document.getElementById("Grade").innerText = "Fail";
+    }
+}
